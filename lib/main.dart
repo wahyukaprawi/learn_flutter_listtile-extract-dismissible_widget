c8c9b6ec70a819553db79f9255ab61e3352f23b0
MacBook <macbook@MacBooks-MacBook-Pro.local>
2024-03-02 02:05:42 +0800

    add Dismissible to delete

--- lib/main.dart
+++ lib/main.dart
@@ -11,23 +11,58 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Learn List Tile Widget'),
-        ),
-        body: ListView.builder(
-          itemCount: 15,
-          itemBuilder: (context, index) {
-            return ListItem(
-              imagesurl: "https://picsum.photos/id/$index/200/300",
-              title:  faker.person.name(),
-              subtitle: faker.lorem.sentence(),
-            );
-          },
-        )
-      )
-    );
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('Learn List Tile Widget'),
+            ),
+            body: ListView.builder(
+              itemCount: 15,
+              itemBuilder: (context, index) {
+                return Dismissible(
+                  confirmDismiss: (direction) {
+                    return showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: Text('CONFIRM'),
+                            content: Text("Are you sure to delete this item"),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                Navigator.of(context).pop(false);
+                                },
+                                child: Text('No')
+                              ),
+                              TextButton(
+                                onPressed: () {
+                                Navigator.of(context).pop(true);
+                                },
+                                child: Text('Yes')
+                              )
+                            ],
+                          );
+                        });
+                  },
+                  key: Key(index.toString()),
+                  direction: DismissDirection.endToStart,
+                  background: Container(
+                    color: Colors.red,
+                    child: Icon(
+                      Icons.delete,
+                      size: 28,
+                    ),
+                    alignment: Alignment.centerRight,
+                    padding: EdgeInsets.only(right: 15),
+                  ),
+                  child: ListItem(
+                    imagesurl: "https://picsum.photos/id/$index/200/300",
+                    title: faker.person.name(),
+                    subtitle: faker.lorem.sentence(),
+                  ),
+                );
+              },
+            )));
   }
 }
 
@@ -35,10 +70,9 @@ class ListItem extends StatelessWidget {
   final String imagesurl;
   final String title;
   final String subtitle;
-  
-  ListItem({
-    required this.imagesurl, required this.title, required this.subtitle
-  });
+
+  ListItem(
+      {required this.imagesurl, required this.title, required this.subtitle});
 
   @override
   Widget build(BuildContext context) {
@@ -47,9 +81,10 @@ class ListItem extends StatelessWidget {
         backgroundImage: NetworkImage(imagesurl),
       ),
       title: Text(title),
-      subtitle: Text(subtitle,
-      maxLines: 1,
-      overflow: TextOverflow.ellipsis,
+      subtitle: Text(
+        subtitle,
+        maxLines: 1,
+        overflow: TextOverflow.ellipsis,
       ),
       trailing: Text('18.00 PM'),
       onTap: () {
